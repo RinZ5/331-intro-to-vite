cf3083f02ae480dcc2f8229e638e927172c03dd1
RinZ <supawit.promma6256@gmail.com>
2025-09-11 23:14:47 +0700

    feat(8): complete 8

--- src/views/event/EventFormView.vue
+++ src/views/event/EventFormView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import EventService from '@/services/EventService';
+import { useMessageStore } from '@/stores/message';
 import type { Event } from '@/types';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
@@ -16,10 +17,15 @@ const event = ref<Event>({
   organizer: ''
 })
 const router = useRouter()
+const store = useMessageStore()
 function saveEvent() {
   EventService.saveEvent(event.value)
     .then((response) => {
       router.push({ name: 'event-detail-view', params: { id: response.data.id } })
+      store.updateMessage('You are successfully add a new event for ' + response.data.title)
+      setTimeout(() => {
+        store.resetMessage()
+      }, 3000)
     })
     .catch(() => {
       router.push({ name: 'network-error-view' })
